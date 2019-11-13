7db36fbc346700e8a1583fc4bd2658868906f82b
jania <jani.aaltonen@outlook.com>
2019-11-13 17:40:00 +0200

    changed add new flower view

--- app/src/main/java/com/example/kasvikullat/AddNewFragment.java
+++ app/src/main/java/com/example/kasvikullat/AddNewFragment.java
@@ -2,39 +2,48 @@ package com.example.kasvikullat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 
-public class AddNewFragment extends Fragment {
+public class AddNewFragment extends Fragment implements FlowerNameAdapter.OnItemClickListener, View.OnClickListener {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference flowerNamesRef;
-    private ArrayList<Flower> flowerNames;
+    private Button buttonSave;
     private String flowerName, flowerName2;
-    FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
     private String userUid;
+    private RecyclerView recyclerView;
     private FlowerNameAdapter adapter;
+    EditText et;
 
     @Nullable
     @Override
@@ -44,14 +53,41 @@ public class AddNewFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         userUid = mAuth.getCurrentUser().getUid();
 
+        buttonSave = view.findViewById(R.id.button_save_fragment_new);
+        buttonSave.setOnClickListener(this);
+        et = view.findViewById(R.id.editText_test);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        buildRecyclerView(view);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().trim().isEmpty()) {
+                    buildRecyclerView();
+                } else {
+                    filteredRecyclerView(editable.toString());
+                }
+            }
+        });
+
+        recyclerView = view.findViewById(R.id.flowerName_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        buildRecyclerView();
 
         return view;
     }
 
 
-    private void buildRecyclerView(View view){
+    private void buildRecyclerView(){
         flowerNamesRef = db.collection("flower_names");
         Query query = flowerNamesRef.orderBy("name", Query.Direction.ASCENDING);
         FirestoreRecyclerOptions<Flower> options = new FirestoreRecyclerOptions.Builder<Flower>()
@@ -59,20 +95,29 @@ public class AddNewFragment extends Fragment {
                 .build();
         adapter = new FlowerNameAdapter(getActivity(), options);
 
-        RecyclerView recyclerView = view.findViewById(R.id.flowerName_recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+        adapter.setOnItemClickListener(this);
+    }
+
+    private void filteredRecyclerView(String string) {
+        String text = string.substring(0,1).toUpperCase() + string.substring(1).toLowerCase();
+        Query query = flowerNamesRef.orderBy("name").startAt(text).endAt(text + "\uf8ff");
+        FirestoreRecyclerOptions<Flower> options = new FirestoreRecyclerOptions.Builder<Flower>()
+                .setQuery(query, Flower.class)
+                .build();
+        adapter = new FlowerNameAdapter(getActivity(), options);
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+        adapter.setOnItemClickListener(this);
+
     }
 
 
     private void saveFlower() {
         long createdAt = System.currentTimeMillis() / 1000;
-
-       /* if (name.trim().isEmpty() || payMethod.trim().isEmpty() || dueDate.trim().isEmpty()) {
-            Toast.makeText(getContext(), "Please fill all the fields", Toast.LENGTH_SHORT).show();
-            return;
-        }*/
 
         CollectionReference flowerRef = FirebaseFirestore.getInstance().collection("users").document(userUid).collection("flowers");
         flowerRef.add(new Flower(flowerName, flowerName2, null, null, 0, 1, 1, createdAt));
@@ -85,12 +130,27 @@ public class AddNewFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        adapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+        Flower flower = documentSnapshot.toObject(Flower.class);
+        flowerName = flower.getName();
+        flowerName2 = flower.getName2();
+        String text = "Lisää " + flowerName + " (" + flowerName2 + ")";
+        buttonSave.setText(text);
+        buttonSave.setEnabled(true);
+        ViewCompat.setBackgroundTintList(buttonSave, ContextCompat.getColorStateList(getActivity(), android.R.color.holo_green_dark));
+    }
+
+    @Override
+    public void onClick(View view) {
+        saveFlower();
     }
 }
